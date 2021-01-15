973b388ff8144539d7ad67fb442f8c34fecc7d11
ahmedmumdouh <ahmedmumdouh@gmail.com>
2021-01-15 11:17:54 +0200

    add dummy testClass in every package

---
+++ ServerSide/src/databaseHandler/testClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databaseHandler;
+
+/**
+ *
+ * @author ahmed
+ */
+public class testClass {
+    
+}
